8dbf741b22e1475128d3efd4275f08768d43bb7c
Mario Alvarado <>
2013-03-02 23:19:47 -0600

    Actualizacion del modulo: Administrador del Sistema

--- resource/js/login.js
+++ resource/js/login.js
@@ -7,12 +7,15 @@ $(document).ready(function(){
         if(user=="admin" && pass=="admin"){
             alert("Bienvenido administrador");
             $(location).attr('href','?mod=admin');
-        }else if(user=="secretaria" && pass=="secreteria"){
-            
+        }else if(user=="secretaria" && pass=="secretaria"){
+            alert("Bienvenido secretaria");
+            $(location).attr('href','?mod=secre');
         }else if(user=="alumno" && pass=="alumno"){
             
         }else if(user=="profesor" && pass=="profesor"){
             
+        }else{
+            alert("Usuario no encontrado");
         }
     });
     
